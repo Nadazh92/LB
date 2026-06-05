5dfcae72d21c10b5e9b4896d9d1f9e6de5226f55
Nadazh92 <nadalizaher92@gmail.com>
2026-06-06 01:01:40 +0200

    FAQ-javascript

--- js/script.js
+++ js/script.js
@@ -1,1 +1,36 @@
 console.log("Hej world");
+
+const questions=document.querySelectorAll(".question");
+
+
+function faq(e){
+// console.log(questions);
+
+const answer = e.currentTarget.nextElementSibling;
+
+
+answer.classList.toggle("active");
+e.currentTarget.classList.toggle("open");
+
+}
+
+
+
+
+
+for (let btn of questions){
+    btn.addEventListener("click",faq);
+    
+}
+
+
+
+
+
+
+
+
+
+
+
+
